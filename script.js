28a55dc9c9690d6b14e7173305277fadd3b2f6ff
Shehryar <shehryarahmed268@gmail.com>
2024-06-24 18:24:45 +0500

    files-upload

---
+++ script.js
@@ -0,0 +1,356 @@
+
+const t20WorldCup2024 = [
+    {
+        groupstage: "T20 1 of 55",
+          date: "Sun, 2 June",
+          match: "USA vs Canada",
+          venue: "Grand Prairie Stadium, Dallas",
+          time: "19:30",
+          flag1: "https://upload.wikimedia.org/wikipedia/commons/d/d9/Flag_of_Canada_%28Pantone%29.svg",
+          flag2: "https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg",
+          result: {
+              winner: "USA",
+              winMargin: "7 wickets",
+              team1: {
+                  name: "Canada",
+                  runs: 147,
+                  overs: 20,
+                  wickets: 8,
+                  extras: 12,
+              },
+              team2: {
+                  name: "USA",
+                  runs: 148,
+                  overs: 18.4,
+                  wickets: 3,
+                  extras: 8,
+              },
+          }
+      },
+      {
+        groupstage: "T20 2 of 55",
+          date: "3 June",
+          match: "Srilanka vs South Africa",
+          venue: "Grand Prairie Stadium, Dallas",
+          time: "19:30",
+          flag1: "https://upload.wikimedia.org/wikipedia/commons/1/11/Flag_of_Sri_Lanka.svg",
+          flag2: "https://upload.wikimedia.org/wikipedia/commons/a/af/Flag_of_South_Africa.svg",
+          result: {
+              winner: "SA",
+              winMargin: "6 wickets",
+              team1: {
+                  name: "Sri Lanka",
+                  runs: 77,
+                  overs: 19.1,
+                  wickets: 10,
+                  extras: 12,
+              },
+              team2: {
+                  name: "South Africa",
+                  runs: 80,
+                  overs: 16.2,
+                  wickets: 4,
+                  extras: 8,
+              },
+          }
+      },
+      {
+        groupstage: "T20 6 of 55",
+          date: "4 June",
+          match: "SCO vs ENG",
+          venue: "Grand Prairie Stadium, Dallas",
+          time: "19:30",
+          flag1: "https://upload.wikimedia.org/wikipedia/commons/1/10/Flag_of_Scotland.svg",
+          flag2: "https://upload.wikimedia.org/wikipedia/en/b/be/Flag_of_England.svg",
+          result: {
+              winner: "No Result",
+              winMargin: "",
+              team1: {
+                  name: "Scotland",
+                  runs: 90,
+                  overs: 10,
+                  wickets: 0,
+                  extras: 12,
+              },
+              team2: {
+                  name: "Eng",
+                  runs: '0',
+                  overs: '0',
+                  wickets:'0',
+                  extras: '',
+              },
+          }
+      },
+      {
+        groupstage: "T20 8 of 55",
+          date: "5 June",
+          match: "IRE vs IND",
+          venue: "Grand Prairie Stadium, Dallas",
+          time: "19:30",
+          flag1: "https://upload.wikimedia.org/wikipedia/commons/4/45/Flag_of_Ireland.svg",
+          flag2: "https://upload.wikimedia.org/wikipedia/en/4/41/Flag_of_India.svg",
+          result: {
+              winner: "IND",
+              winMargin: "8 wickets",
+              team1: {
+                  name: "Ireland",
+                  runs: 96,
+                  overs: 16,
+                  wickets: 10,
+                  extras: 12,
+              },
+              team2: {
+                  name: "India",
+                  runs: 97,
+                  overs: 12.2,
+                  wickets: 2,
+                  extras: 8,
+              },
+          }
+      },
+      {
+        groupstage: "T20 11 of 55",
+          date: "6 June",
+          match: "PAK vs USA",
+          venue: "Grand Prairie Stadium, Dallas",
+          time: "19:30",
+          flag1: "https://upload.wikimedia.org/wikipedia/commons/3/32/Flag_of_Pakistan.svg",
+          flag2: "https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg",
+          result: {
+              winner: "USA",
+              winMargin: "5 Runs",
+              team1: {
+                  name: "Pakistan",
+                  runs: `159`,
+                  overs: '20',
+                  wickets: 7,
+                  extras: 12,
+              },
+              team2: {
+                  name: "United States",
+                  runs: 159,
+                  overs: 20,
+                  wickets: 4,
+                  extras: 8,
+              },
+          }
+      },
+      {
+        groupstage: "T20 14 of 55",
+          date: "8 June",
+          match: "AFG vs NZ",
+          venue: "Grand Prairie Stadium, Dallas",
+          time: "19:30",
+          flag1: "https://ssl.gstatic.com/onebox/media/sports/logos/8rwaXVaUKfdJq31QgGtQrw_96x96.png",
+          flag2: "https://upload.wikimedia.org/wikipedia/commons/3/3e/Flag_of_New_Zealand.svg",
+          result: {
+              winner: "AFG",
+              winMargin: "84 Runs",
+              team1: {
+                  name: "Afghanistan",
+                  runs: 159,
+                  overs: 20,
+                  wickets: 6,
+                  extras: 12,
+              },
+              team2: {
+                  name: "New Zealand",
+                  runs: 75,
+                  overs: 15.2,
+                  wickets: 10,
+                  extras: 8,
+              },
+          }
+      },
+      {
+        groupstage: "T20 17 of 55",
+          date: "8 June",
+          match: "Australia vs England",
+          venue: "Grand Prairie Stadium, Dallas",
+          time: "19:30",
+          flag1: "https://upload.wikimedia.org/wikipedia/en/b/b9/Flag_of_Australia.svg",
+          flag2: "https://upload.wikimedia.org/wikipedia/en/b/be/Flag_of_England.svg",
+          result: {
+              winner: "AUS",
+              winMargin: "36 Runs",
+              team1: {
+                  name: "Australia",
+                  runs: 201,
+                  overs: 20,
+                  wickets: 7,
+                  extras: 12,
+              },
+              team2: {
+                  name: "England",
+                  runs: 165,
+                  overs: 20,
+                  wickets: 6,
+                  extras: 8,
+              },
+          }
+      },
+      {
+        groupstage: "T20 19 of 55",
+          date: "9 June",
+          match: "India vs Pakistan",
+          venue: "Grand Prairie Stadium, Dallas",
+          time: "19:30",
+          flag1: "https://upload.wikimedia.org/wikipedia/en/4/41/Flag_of_India.svg",
+          flag2: "https://upload.wikimedia.org/wikipedia/commons/3/32/Flag_of_Pakistan.svg",
+          result: {
+              winner: "IND",
+              winMargin: "6 Runs",
+              team1: {
+                  name: "India",
+                  runs: 119,
+                  overs: 19,
+                  wickets: 10,
+                  extras: 12,
+              },
+              team2: {
+                  name: "Pakistan",
+                  runs: 113,
+                  overs: 20,
+                  wickets: 7,
+                  extras: 8,
+              },
+          }
+      },
+      {
+        groupstage: "T20 21 of 55",
+          date: "10 June",
+          match: "SA vs BAN",
+          venue: "Grand Prairie Stadium, Dallas",
+          time: "19:30",
+          flag1: "https://upload.wikimedia.org/wikipedia/commons/a/af/Flag_of_South_Africa.svg",
+          flag2: "https://upload.wikimedia.org/wikipedia/commons/f/f9/Flag_of_Bangladesh.svg",
+          result: {
+              winner: "SA",
+              winMargin: "4 Runs",
+              team1: {
+                  name: "South Africa",
+                  runs: 113,
+                  overs: 20,
+                  wickets: 6,
+                  extras: 12,
+              },
+              team2: {
+                  name: "Bangladesh",
+                  runs: 109,
+                  overs: 20,
+                  wickets: 7,
+                  extras: 8,
+              },
+          }
+      },
+      {
+        groupstage: "T20 22 of 55",
+          date: "11 June",
+          match: "Canada vs Pakistan",
+          venue: "Grand Prairie Stadium, Dallas",
+          time: "19:30",
+          flag1: "https://upload.wikimedia.org/wikipedia/commons/d/d9/Flag_of_Canada_%28Pantone%29.svg",
+          flag2: "https://upload.wikimedia.org/wikipedia/commons/3/32/Flag_of_Pakistan.svg",
+          result: {
+              winner: "Pakistan",
+              winMargin: "7 Wickets",
+              team1: {
+                  name: "Canada",
+                  runs: 106,
+                  overs: 20,
+                  wickets: 7,
+                  extras: 12,
+              },
+              team2: {
+                  name: "Pakistan",
+                  runs: 107,
+                  overs: 17.3,
+                  wickets: 3,
+                  extras: 8,
+              },
+          }
+      },
+      {
+        groupstage: "T20 26 of 55",
+          date: "13 June",
+          match: "WES vs NZ",
+          venue: "Grand Prairie Stadium, Dallas",
+          time: "19:30",
+          flag1: "https://ssl.gstatic.com/onebox/media/sports/logos/6om0X7vSyKW3Lj4AVpqWIQ_48x48.png",
+          flag2: "https://upload.wikimedia.org/wikipedia/commons/3/3e/Flag_of_New_Zealand.svg",
+          result: {
+              winner: "WI",
+              winMargin: "13 Runs",
+              team1: {
+                  name: "West Indies",
+                  runs: 149,
+                  overs: 20,
+                  wickets: 9,
+                  extras: 12,
+              },
+              team2: {
+                  name: "New Zealand",
+                  runs: 136,
+                  overs: 20,
+                  wickets: 9,
+                  extras: 8,
+              },
+          }
+      },
+      {
+        groupstage: "T20 43 of 55",
+          date: "20 June",
+          match: "IND vs AFG",
+          venue: "Grand Prairie Stadium, Dallas",
+          time: "19:30",
+          flag1: "https://upload.wikimedia.org/wikipedia/en/4/41/Flag_of_India.svg",
+          flag2: "https://ssl.gstatic.com/onebox/media/sports/logos/8rwaXVaUKfdJq31QgGtQrw_48x48.png",
+          result: {
+              winner: "IND",
+              winMargin: "47 Runs",
+              team1: {
+                  name: "India",
+                  runs: 181,
+                  overs: 20,
+                  wickets: 8,
+                  extras: 12,
+              },
+              team2: {
+                  name: "Afghanistan",
+                  runs: 134,
+                  overs: 20,
+                  wickets: 10,
+                  extras: 8,
+              },
+          }
+      },
+]
+ 
+let arr = t20WorldCup2024.map((match) => {
+    let main = document.querySelector('.matchboxes')
+        let box = document.createElement('div')
+        box.className='box'
+        box.innerHTML = `<div class="level-1 flex">
+        <div class="group-stage-1 grey">Group Stage: ${match.groupstage}</div>
+        <div class="date-1 grey">${match.date}</div>
+    </div>
+    <div class="level-2 flex">
+        <div class="team">
+            <div class="can-flag teamflag"><img src="${match.flag1}" class='flag' alt=""><span>${match.result.team1.name}</span></div>
+        </div>
+        <div class="can-score">${match.result.team1.runs}/${match.result.team1.wickets} (${match.result.team1.overs})</div>
+    </div>
+    <div class="level-3 flex">
+        <div class="team">
+    
+            <div class="usa-flag teamflag"><img src="${match.flag2}" class='flag' alt=""> <span>${match.result.team2.name}</span></div>
+           
+        </div>
+        <div class="usa-score">${match.result.team2.runs}/${match.result.team2.wickets} (${match.result.team2.overs})</div>
+    </div>
+    <div class="level-4 flex">
+        <div class="des-1">${match.result.winner} won by ${match.result.winMargin}</div>
+        <div class="match-pic"></div>
+    </div>`
+    main.appendChild(box);  
+})
